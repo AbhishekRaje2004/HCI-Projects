2b0b1c43cf2ff1c5087a10caf64347ac786b6fed
Abhishek Raje <118584747+AbhishekRaje2004@users.noreply.github.com>
2024-05-15 18:50:09 +0530

    Update ClawController.ino

--- ClawController/ClawController.ino
+++ ClawController/ClawController.ino
@@ -1,30 +1,3 @@
-// Claw Controller - BioAmp EXG Pill
-// Use for Bionic (wolverine) claw as well
-// https://github.com/upsidedownlabs/BioAmp-EXG-Pill
-
-// Upside Down Labs invests time and resources providing this open source code,
-// please support Upside Down Labs and open-source hardware by purchasing
-// products from Upside Down Labs!
-
-// Copyright (c) 2021 - 2023 Upside Down Labs - contact@upsidedownlabs.tech
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
 
 #if defined(ESP32) 
   // For ESP32 Servo library
