22324a984ba2ae272cfcf88be555b18b72aefa84
Abhishek Raje <118584747+AbhishekRaje2004@users.noreply.github.com>
2024-05-17 00:41:47 +0530

    Add files via upload
    Modelling Synapse

---
+++ Stochastic Modelling Neuron/synaptic_dynamics.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def update_synapses(synapses, base_efficiency=0.1, variability=0.03):
+    """Update synaptic efficiencies with stochastic variability."""
+    new_weights = base_efficiency + np.random.normal(0, variability, size=len(synapses))
+    synapses.w = new_weights
+
