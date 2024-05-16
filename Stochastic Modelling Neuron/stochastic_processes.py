fe6801840022ec781c802ea6b07d00c6cd7b95e1
Abhishek Raje <118584747+AbhishekRaje2004@users.noreply.github.com>
2024-05-17 00:43:11 +0530

    Add files via upload

---
+++ Stochastic Modelling Neuron/stochastic_processes.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def stochastic_activation(neurons, activation_rate=0.01):
+    """Randomly activate neurons based on a specified rate."""
+    random_activations = np.random.binomial(1, activation_rate, size=len(neurons))
+    neurons.v += random_activations
+
