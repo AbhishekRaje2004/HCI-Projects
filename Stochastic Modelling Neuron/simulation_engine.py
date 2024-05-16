060d5f0e1624d2f13849259f1bd14b9b61097865
Abhishek Raje <118584747+AbhishekRaje2004@users.noreply.github.com>
2024-05-17 00:44:16 +0530

    Add files via upload

---
+++ Stochastic Modelling Neuron/simulation_engine.py
@@ -0,0 +1,13 @@
+from brian2 import *	
+
+from brian2 import run
+from neural_network import create_neural_network
+from synaptic_dynamics import update_synapses
+from stochastic_processes import stochastic_activation
+
+def run_simulation(duration=1*second):
+    neurons, synapses = create_neural_network()
+    net = Network(neurons, synapses)
+    net.run(duration)
+    return net
+
